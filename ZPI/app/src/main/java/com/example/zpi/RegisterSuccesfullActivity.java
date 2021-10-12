d7ef3f2c2a0f81428604593f75fc35f20a115575
Adam <adam.adam199989@gmail.com>
2021-10-12 16:47:44 +0200

    powrto do logowania po rejestracji

--- ZPI/app/src/main/java/com/example/zpi/RegisterSuccesfullActivity.java
+++ ZPI/app/src/main/java/com/example/zpi/RegisterSuccesfullActivity.java
@@ -2,7 +2,9 @@ package com.example.zpi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterSuccesfullActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegisterSuccesfullActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.successful_register);
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
