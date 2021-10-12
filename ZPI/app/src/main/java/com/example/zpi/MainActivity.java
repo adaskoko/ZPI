f6752ce425942bff5a81a7f7e200f2bbe0eeb067
Adam <adam.adam199989@gmail.com>
2021-10-12 08:50:51 +0200

    Login activity

--- ZPI/app/src/main/java/com/example/zpi/MainActivity.java
+++ ZPI/app/src/main/java/com/example/zpi/MainActivity.java
@@ -27,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //new GetExample().execute();
-
         User user = new User();
         user.setEmail("test_dao@test.com");
         user.setBirthDate(new Date());
@@ -45,74 +43,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }).start();
 
-
-    }
-
-    private class SaveExample extends AsyncTask<Void, Void, Void>{
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            String databaseUrl = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11439521";
-            try {
-                ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl, "sql11439521", "wdsA5B6LFC");
-                Dao<User, String> userDao = DaoManager.createDao(connectionSource, User.class);
-
-                User user = new User();
-                user.setEmail("test2@test.com");
-                user.setBirthDate(new Date());
-                user.setJoiningDate(new Date());
-                user.setName("test name");
-                user.setPassword("test password");
-                user.setSurname("test surname");
-
-                Dao<ProductToTake, String> productDao = DaoManager.createDao(connectionSource, ProductToTake.class);
-                ProductToTake product = new ProductToTake();
-                product.setUser(user);
-                product.setAmmount(3);
-                product.setName("testowy produkt");
-
-                userDao.create(user);
-                productDao.create(product);
-
-                connectionSource.close();
-
-            } catch (SQLException | IOException throwables) {
-                throwables.printStackTrace();
-            }
-            return null;
-        }
-    }
-
-    private class GetExample extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            String databaseUrl = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11439521";
-            try {
-                ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl, "sql11439521", "wdsA5B6LFC");
-
-                Dao<ProductToTake, String> productDao = DaoManager.createDao(connectionSource, ProductToTake.class);
-
-                QueryBuilder<ProductToTake, String> queryBuilder = productDao.queryBuilder();
-                queryBuilder.where().eq("Name", "testowy produkt");
-                List<ProductToTake> products = queryBuilder.query();
-                ProductToTake product = products.get(0);
-
-                Dao<User, String> userDao = DaoManager.createDao(connectionSource, User.class);
-                User user = product.getUser();
-                userDao.refresh(user);
-
-                Log.i("product", product.getName() + ' ' + product.getAmmount());
-                Log.i("user", user.getEmail() + ' ' + user.getName());
-
-                connectionSource.close();
-
-            } catch (SQLException | IOException throwables) {
-                throwables.printStackTrace();
-            }
-            return null;
-        }
     }
 }
