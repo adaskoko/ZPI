e4885d99b0511447c2bf291ff3c64a3ef452a8ba
Weronika Strączek <w.straczek@campus.fct.unl.pt>
2021-11-16 17:12:19 +0100

    porzondki

--- ZPI/app/src/main/java/com/example/zpi/bottomnavigation/ui/trip/TripMainpanelViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.zpi.bottomnavigation.ui.trip;
-
-import androidx.lifecycle.ViewModel;
-
-public class TripMainpanelViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
