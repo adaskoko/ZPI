c18bfbbaaecb523ea8997616d79211a7160ea359
Weronika Strączek <w.straczek@campus.fct.unl.pt>
2021-11-27 17:33:52 +0100

    whatever

--- ZPI/app/src/main/java/com/example/zpi/ForumActivity.java
+++ ZPI/app/src/main/java/com/example/zpi/ForumActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.zpi.data_handling.BaseConnection;
